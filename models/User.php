328b5235a24792387e588b359a76affb2dadd147
robregonm <robregonm@gmail.com>
2015-05-18 20:52:21 -0500

    [FIX] Adds setPassword function (was missing)

--- models/User.php
+++ models/User.php
@@ -33,11 +33,6 @@ class User extends ActiveRecord implements IdentityInterface
 	const STATUS_ACTIVE = 2;
 	const STATUS_SUSPENDED = 3;
 
-	/**
-	 * @var string the raw password. Used to collect password input and isn't saved in database
-	 */
-	public $password;
-
 	private $_isSuperAdmin = null;
 
 	private $statuses = [
@@ -150,14 +145,26 @@ class User extends ActiveRecord implements IdentityInterface
 		return $this->auth_key === $authKey;
 	}
 
-	/**
-	 * @param string $password password to validate
-	 * @return bool if password provided is valid for current user
-	 */
-	public function validatePassword($password)
-	{
-		return Yii::$app->getSecurity()->validatePassword($password, $this->password_hash);
-	}
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
 
 	/**
 	 * @inheritdoc
