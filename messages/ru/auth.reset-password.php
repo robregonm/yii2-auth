c12692121283d60d256638415217f450a359adec
MikeBelozorov <mike@belozorov63.ru>
2015-07-01 14:43:38 +0300

    Add russian language

---
+++ messages/ru/auth.reset-password.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Florian Werner <fwerner13@gmail.com>
+ * @created 2014-12-13
+ */
+return [
+	'Request password reset' => 'Запрос на смену пароля',
+	'Please fill out your email. A link to reset password will be sent there.' => 'Пожалуйста введите почтовый ящик. На него будет выслана ссылка необходимая для смены пароля.',
+	'There is no user with such email.' => 'Нет пользователей с таким почтовым ящиком.',
+	'Password reset token cannot be blank.' => 'Токен не может быть пустым.',
+	'Wrong password reset token.' => 'Неверный токен.',
+	'Send' => 'Отправить',
+	'Password reset for {name}' => 'Сброс пароля для {name}',
+	'Reset password' => 'Сброс пароля',
+	'Please choose your new password' => 'Выберите новый пароль',
+	'Save' => 'Сохранить',
+];
---
+++ messages/ru/auth.user.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Florian Werner <fwerner13@gmail.com>
+ * @created 2014-12-13
+ */
+return [
+	'Username' => 'Имя пользователя',
+	'Email' => 'Email',
+	'Password' => 'Пароль',
+	'Status' => 'Статус',
+	'Last Visit Time' => 'Время последнего визита',
+	'Create Time' => 'Время создания',
+	'Update Time' => 'Время последних изменений',
+	'Delete Time' => 'Время удаления',
+	'Incorrect username or password.' => 'Неверное имя пользователя или пароль.',
+	'Profile' => 'Профиль',
+	'View Profile' => 'Просмотр профиля',
+	'Update Profile' => 'Редактировать профиль',
+	'Login' => 'Войти',
+	'Sign up' => 'Регистрация',
+	'Logout' => 'Выйти',
+	'Username or Email' => 'Имя пользователя или Email',
+	'Remember Me' => 'Запомнить меня',
+	'Please fill out the following fields to login:' => 'Пожалуйста заполните следующие поля:',
+	'Create' => 'Создать',
+	'Create User' => 'Создать пользователя',
+	'Users' => 'Пользователи',
+	'Update' => 'Редактировать',
+	'Update User' => 'Редактировать пользователя',
+	'Delete' => 'Удалить',
+	'Deleted' => 'Удален',
+	'Inactive' => 'Не активен',
+	'Active' => 'Активен',
+	'Suspended' => 'Выключен',
+	'Forgot password?' => 'Забыли пароль?',
+	'Already registered? Sign in!' => 'Уже зарегистрированы? Входите!',
+];
